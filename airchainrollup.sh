b0f322672c07b6e5c4f8b2ec114ebeac7b565a09
candy1264 <137707882+candy1264@users.noreply.github.com>
2024-06-14 20:27:36 +0800

    Update airchainrollup.sh

--- airchainrollup.sh
+++ airchainrollup.sh
@@ -328,7 +328,7 @@ function tracks_log(){
 }
 function private_key(){
     #evmos私钥#
-    /bin/bash ./scripts/local-keys.sh
+    /bin/bash /data/airchains/evm-station/scripts/scripts/local-keys.sh
     #avail助记词#
     cat /root/.avail/identity/identity.toml
     #airchain助记词#
