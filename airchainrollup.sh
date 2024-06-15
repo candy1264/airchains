c9f48ad3f5e17dd1facec315b8de896af181a428
candy1264 <137707882+candy1264@users.noreply.github.com>
2024-06-16 07:35:26 +0800

    Update airchainrollup.sh

--- airchainrollup.sh
+++ airchainrollup.sh
@@ -302,6 +302,12 @@ function check_avail_address(){
 journalctl -u availd |head 
 }
 
+function restart(){
+sudo systemctl restart evmosd
+sudo systemctl restart availd 
+sudo systemctl restart tracksd
+}
+
 function delete_node(){
 rm -rf data
 rm -rf .evmosd
@@ -317,6 +323,7 @@ sudo pkill -9 availd
 sudo pkill -9 evmosd
 sudo pkill -9 tracksd
 }
+
 # 主菜单
 function main_menu() {
     while true; do
@@ -335,7 +342,8 @@ function main_menu() {
         echo "4. 查看tracks状态"
         echo "5. 导出所有私钥"
         echo "6. 查看avail地址"
-        echo "7. 删除节点"
+        echo "7. 重启服务"
+        echo "8. 删除节点"
         read -p "请输入选项（1-11）: " OPTION
 
         case $OPTION in
@@ -345,7 +353,8 @@ function main_menu() {
         4) tracks_log ;;
         5) private_key ;;
         6) check_avail_address ;;
-        7) delete_node ;;
+        7) restart ;;
+        8) delete_node ;;
         *) echo "无效选项。" ;;
         esac
         echo "按任意键返回主菜单..."
