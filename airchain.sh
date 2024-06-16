db68f13c028a7c825f6af0d5a12c20172934a86d
candy1264 <137707882+candy1264@users.noreply.github.com>
2024-06-16 13:34:59 +0800

    Update airchain.sh

--- airchain.sh
+++ airchain.sh
@@ -1,5 +1,4 @@
 docker run -d --name airchains ubuntu:20.04 sleep infinity
-docker exec -it airchains bash << EOF
+docker exec -it airchains bash -c "
 wget -O airchainrollup.sh https://raw.githubusercontent.com/candy1264/airchains/main/airchainrollup.sh && chmod +x airchainrollup.sh && ./airchainrollup.sh
-            exit
-EOF
+"
