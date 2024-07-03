72b7a006ade21657ef293267c0c59ff6b907bc6f
candy1264 <137707882+candy1264@users.noreply.github.com>
2024-07-03 10:21:27 +0800

    Update airchainevm.sh

--- airchainevm.sh
+++ airchainevm.sh
@@ -80,15 +80,9 @@ else
     sudo apt install -y wget
 fi
 
-# 检查是否已安装 go
-if command -v go >/dev/null 2>&1; then
-    echo "go 已安装，跳过安装步骤。"
-else
-    echo "下载并安装 Go..."
     wget -c https://golang.org/dl/go1.22.3.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
     echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
     source ~/.bashrc
-fi
 
 # 验证安装后的 Go 版本
 echo "当前 Go 版本："
