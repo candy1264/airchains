969ea355b47afab41f3ba33ed4859e078894d54d
candy1264 <137707882+candy1264@users.noreply.github.com>
2024-07-04 23:15:44 +0800

    Update restart991.sh

--- restart991.sh
+++ restart991.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 监控的错误信息
-ERROR_STRINGS=("cosmos/cosmos-sdk@v0.50.3/baseapp/baseapp.go:991" "Failed to Init VRF")
+ERROR_STRING_1="cosmos/cosmos-sdk@v0.50.3/baseapp/baseapp.go:991"
+ERROR_STRING_2="Failed to Init VRF"
 
 LOG_FILE="$HOME/logfile.log"
 
@@ -15,16 +16,21 @@ JOURNALCTL_PID=$!
 
 # 循环检查日志文件中是否包含错误字符串
 while true; do
-  # 仅读取日志文件的最后50行
-  TAIL_LINES=$(tail -n 50 "$LOG_FILE")
+  # 仅读取日志文件的最新3行来检测第一个错误字符串
+  TAIL_LINES_3=$(tail -n 3 "$LOG_FILE")
   
-  for ERROR_STRING in "${ERROR_STRINGS[@]}"; do
-    if echo "$TAIL_LINES" | grep -q "$ERROR_STRING"; then
-      echo "检测到错误信息 '$ERROR_STRING'，重启 stationd 服务..."
-      sudo systemctl restart stationd
-      break
-    fi
-  done
-  
+  if echo "$TAIL_LINES_3" | grep -q "$ERROR_STRING_1"; then
+    echo "检测到错误信息 '$ERROR_STRING_1'，重启 stationd 服务..."
+    sudo systemctl restart stationd
+  fi
+
+  # 仅读取日志文件的最新100行来检测第二个错误字符串
+  TAIL_LINES_100=$(tail -n 100 "$LOG_FILE")
+
+  if echo "$TAIL_LINES_100" | grep -q "$ERROR_STRING_2"; then
+    echo "检测到错误信息 '$ERROR_STRING_2'，重启 stationd 服务..."
+    sudo systemctl restart stationd
+  fi
+
   sleep 60
 done
