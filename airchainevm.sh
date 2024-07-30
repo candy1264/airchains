906a19491d711bd697646e23b06d001587588318
candy1264 <137707882+candy1264@users.noreply.github.com>
2024-07-30 15:39:43 +0800

    Update airchainevm.sh

--- airchainevm.sh
+++ airchainevm.sh
@@ -115,7 +115,7 @@ EOF
     NODE_ID=$(grep 'node_id =' $CONFIG_PATH | awk -F'"' '{print $2}')
     LOCAL_IP=$(hostname -I | awk '{print $1}')
 
-    create_station_cmd="go run cmd/main.go create-station --accountName wallet --accountPath $HOME/.tracks/junction-accounts/keys --jsonRPC \"https://airchains-rpc.kubenode.xyz/\" --info \"EVM Track\" --tracks \"$AIR_ADDRESS\" --bootstrapNode \"/ip4/$LOCAL_IP/tcp/2300/p2p/$NODE_ID\""
+    create_station_cmd="go run cmd/main.go create-station --accountName wallet --accountPath $HOME/.tracks/junction-accounts/keys --jsonRPC \"https://airchains-rpc-testnet.zulnaaa.com/\" --info \"EVM Track\" --tracks \"$AIR_ADDRESS\" --bootstrapNode \"/ip4/$LOCAL_IP/tcp/2300/p2p/$NODE_ID\""
 
     echo "Running command:"
     echo "$create_station_cmd"
